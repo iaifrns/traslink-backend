09a6242ad01739d41f2549b8aba2ffc35732361c
iaifrns <nsinifranc@gmail.com>
2025-06-03 17:59:48 +0100

    first push

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,37 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 import { CreateUser } from './dto/createUser';
-import { Login } from './dto/login';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+    constructor(@InjectRepository(User) private userRepo: Repository<User>){}
 
-  getAllUsers() {
-    return this.userRepo.find();
-  }
+    findUserByUsername(username: string){
+        return this.userRepo.findOneBy({username})
+    }
 
-  getUserById(id: number) {
-    return this.userRepo.findBy({ id: id });
-  }
-
-  async checkIfUserExist(credentials: Login) {
-    const user = await this.userRepo.findOneBy({
-      username: credentials.username,
-      password: credentials.password,
-    });
-    return user;
-  }
-
-  create(createUser: CreateUser) {
-    const user = this.userRepo.create(createUser);
-    const response = this.userRepo.save(user);
-    return response;
-  }
-
-  update(user: User) {
-    return this.userRepo.save(user);
-  }
+    async createUser(createUser: CreateUser){
+        const password = await bcrypt.hash(createUser.password, 10)
+        const newUser = this.userRepo.create({...createUser, password: password})
+        return this.userRepo.save(newUser)
+    }
 }
